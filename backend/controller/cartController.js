d525230a3444cb3be13906032b8df57438c58dad
Umer Jamil Khan <umerjamilkhan268@gmail.com>
2026-07-09 02:25:34 +0530

    feat:implement complete cart management Apis

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -100,13 +100,140 @@ export const getCartItems = async (req,res) =>{
     return res.status(200).json({
         success:true,
         message:"Get all cart items",
-        carts
+        cart:carts
     })
 }catch(err){
     console.log(err.message);
     return res.status(500).json({
         success:false,
         message:"Internal Server Error"
+    })
+}
+}
+
+export const updateBookQuantity = async (req,res)=>{
+    try{
+
+    const {quantity} = req.body;
+    const {bookId} = req.params
+    if(!mongoose.Types.ObjectId.isValid(bookId)){
+        return res.status(400).json(
+          {
+            success:false,
+            message:"Book id is not valid "
+          }
+        )
+    }
+    if(quantity<1) {
+        return res.status(400).json({
+            success:false,
+            message:"Quantity must be atleast 1"
+        })
+    }
+
+    const updatedCart = await Cart.findOneAndUpdate({
+        user :req.user.userId,
+        "items.book":bookId
+    },{
+        $set:{
+            "items.$.quantity":quantity
+        }
+    },{
+        returnDocument:'after'
+    })
+    if(!updatedCart){
+        return res.status(404).json({
+            success:false,
+            message:"Book is not in the cart"
+        })
+    }
+    return res.status(200).json({
+        success:true,
+        message:"Quantity updated successfully",
+        cart:updatedCart
+    })
+}catch(err){
+    console.log(err.message);
+    return res.status(500).json({
+        success:false,
+        message:"Internal server error"
+    })
+}
+}
+
+export const deleteSingleCartItem = async (req,res) =>{
+    try{
+    const {bookId} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(bookId)){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid Book Id"
+        })
+    }
+    const cart = await Cart.findOne({
+        user:req.user.userId,
+        "items.book":bookId
+    })
+    if (!cart) {
+    return res.status(404).json({
+        success: false,
+        message: "Book doesn't exist in cart"
+    });
+}
+    const deletedCartItem = await Cart.findOneAndUpdate({
+        user:req.user.userId,
+    },{
+        $pull: {
+            items: {
+                book: bookId
+            }
+        }
+    },{
+        returnDocument:'after'
+    })
+    return res.status(200).json({
+        success:true,
+        message:"Book Deleted Successfully",
+        cart : deletedCartItem
+    })
+}catch(err){
+    console.log(err.message);
+    return res.status(500).json({
+        success:false,
+        message:"Internal server error"
+    })
+}
+}
+
+export const deleteCart = async (req,res)=>{
+    try{
+    const user = req.user.userId;
+
+    const deletedCartItem = await Cart.findOneAndUpdate({
+        user
+    },{
+        $set:{
+            items:[]
+        }
+    },{
+        returnDocument:'after'
+    })
+    if (!deletedCartItem) {
+    return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+        cart : deletedCartItem
+    });
+}
+    return res.status(200).json({
+        success:true,
+        message:"Cart Deleted Successfully"
+    })
+}catch(err){
+    console.log(err.message);
+    return res.status(500).json({
+        success:false,
+        message:"Internal server error"
     })
 }
 }
--- backend/route/cartRoute.js
+++ backend/route/cartRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { auth } from '../middleware/authMiddleware.js'
-import { createCartItem , getCartItems} from '../controller/cartController.js';
+import { createCartItem , deleteCart, deleteSingleCartItem, getCartItems, updateBookQuantity} from '../controller/cartController.js';
 
 const router = express.Router()
 
 router.post("/",auth,createCartItem);
-router.get("/",auth,getCartItems)
-
+router.get("/",auth,getCartItems);
+router.put("/:bookId",auth,updateBookQuantity);
+router.delete("/:bookId",auth,deleteSingleCartItem)
+router.delete("/",auth,deleteCart)
 export default router;
